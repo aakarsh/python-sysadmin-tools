4e75f56500035ee49bb1e604a001ff975f0d1781
Aakarsh Nair <aakarsh.nair@gmail.com>
2018-03-25 02:29:34 +0000

    adding meminfo

--- src/nix_sys_info.py
+++ src/nix_sys_info.py
@@ -20,9 +20,18 @@ def disk_info():
 	return file_systems
 
 
+
+def meminfo():
+	res = subprocess.Popen(["cat","/proc/meminfo"],4096,stdout=subprocess.PIPE)
+	field_values = [line.strip().split(":") for line in res.stdout.readlines()]
+	ret = {}
+	for fv in field_values:
+		ret[fv[0].strip().lower()] = fv[1].strip()
+	return ret
+
 sys_info = {}
 sys_info["disk_info"] = disk_info()
-
+sys_info["mem_info"] = meminfo()
 json_string = json.dumps(sys_info,indent=True)		
 print(json_string)
 	
